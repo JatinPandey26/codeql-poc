e4af2f79f7a3c1443b05c1105b5b7228d44da97d
Jatin <jatin26pandey@gmail.com>
2023-12-15 18:14:10 +0530

    try1

--- src/main/java/com.kipu_fav.write_module/Controllers/Controller.java
+++ src/main/java/com.kipu_fav.write_module/Controllers/Controller.java
@@ -3,12 +3,14 @@ package com.kipu_fav.write_module.Controllers;
 import com.kipu_fav.write_module.Entity.Booking;
 import com.kipu_fav.write_module.Entity.Schedule;
 import com.kipu_fav.write_module.Service.Schedule_service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/schedule")
 public class Controller {
     @Autowired
@@ -16,6 +18,7 @@ public class Controller {
 
     @PostMapping("/new")
     public String createSchedule(@RequestBody Schedule schedule){
+        log.info("schedule created" + schedule);
         this.schedule_service.saveSchedule(schedule);
         return "schedule created!!!";
     }
